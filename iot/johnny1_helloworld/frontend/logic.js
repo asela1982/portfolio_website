55c7a6de682b3346e3a4e1ffae85b5de30f525c8
asela1982 <asela.d82@gmail.com>
2018-02-14 13:07:07 -0600

    iot prj2_blinkLED

---
+++ iot/johnny1_helloworld/frontend/logic.js
@@ -0,0 +1,26 @@
+
+
+// initialize a PubNub instance.
+var pubnub = new PubNub({
+    subscribeKey: 'pub-c-88530e4b-9d85-4bcb-86ac-2019a7d19c24', // always required
+    publishKey: 'sub-c-f58342d2-1158-11e8-a78c-ded3dc92678b' // only required if publishing
+});
+
+
+var $btn = document.querySelector("button.on");
+var channel = 'blinkLED';
+var blinkState = true;
+
+
+$btn.addEventListener('click', function (e) {
+    // publishing
+    pubnub.publish({
+            channel: channel,
+            message: {blink: blinkState},
+            callback: function(m){
+                console.log(m);
+                blinkState = !blinkState;
+                $btn.textContent = (blinkState) ? 'Blink LED' : 'Stop LED';
+            }
+        });
+});
